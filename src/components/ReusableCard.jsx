e7a7a3983f444a7e1f4b7cfcb42cbde983a04d4a
Owen <owenkaris9512@gmail.com>
2025-04-22 21:43:21 +0300

    not done

---
+++ src/components/ReusableCard.jsx
@@ -0,0 +1,16 @@
+import { Card } from "@/components/ui/card";
+
+function Card({name, movies}) {
+    const movieList = movies.map(movie => <li key={movie}>{movie}</li>)
+    
+    return (
+      <article>
+          <h2>{name}</h2>
+          <ul>
+              {movieList}
+          </ul>
+      </article>
+    )
+  }
+  
+  export default Card;
